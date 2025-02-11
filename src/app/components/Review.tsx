63b3245780c0696785cd43fd5d23890531ba3247
Marco Dave <marcodave03@gmail.com>
2025-02-11 13:58:31 +0700

    .

--- src/app/components/Review.tsx
+++ src/app/components/Review.tsx
@@ -1,25 +1,24 @@
 import Image from 'next/image';
 import React from 'react';
 import Review1 from "../../../assets/Review/review (1).svg"
-import Review2 from "../../../assets/Review/review (1).svg"
-import Review3 from "../../../assets/Review/review (1).svg"
-import Review4 from "../../../assets/Review/review (1).svg"
-import Review5 from "../../../assets/Review/review (1).svg"
-import Review6 from "../../../assets/Review/review (1).svg"
-import Review7 from "../../../assets/Review/review (1).svg"
-import Review8 from "../../../assets/Review/review (1).svg"
-import Review9 from "../../../assets/Review/review (1).svg"
-import Review10 from "../../../assets/Review/review (1).svg"
-import Review11 from "../../../assets/Review/review (1).svg"
-import Review12 from "../../../assets/Review/review (1).svg"
-import Review13 from "../../../assets/Review/review (1).svg"
-import Review13 from "../../../assets/Review/review (1).svg"
-import Review1 from "../../../assets/Review/review (1).svg"
-import Review1 from "../../../assets/Review/review (1).svg"
-import Review1 from "../../../assets/Review/review (1).svg"
-import Review1 from "../../../assets/Review/review (1).svg"
-import Review1 from "../../../assets/Review/review (1).svg"
-import Review1 from "../../../assets/Review/review (1).svg"
+import Review2 from "../../../assets/Review/review (2).svg"
+import Review3 from "../../../assets/Review/review (3).svg"
+import Review4 from "../../../assets/Review/review (4).svg"
+import Review5 from "../../../assets/Review/review (5).svg"
+import Review6 from "../../../assets/Review/review (6).svg"
+import Review7 from "../../../assets/Review/review (7).svg"
+import Review8 from "../../../assets/Review/review (8).svg"
+import Review9 from "../../../assets/Review/review (9).svg"
+import Review10 from "../../../assets/Review/review (10).svg"
+import Review11 from "../../../assets/Review/review (11).svg"
+import Review12 from "../../../assets/Review/review (12).svg"
+import Review13 from "../../../assets/Review/review (13).svg"
+import Review14 from "../../../assets/Review/review (14).svg"
+import Review15 from "../../../assets/Review/review (15).svg"
+import Review16 from "../../../assets/Review/review (16).svg"
+import Review17 from "../../../assets/Review/review (17).svg"
+import Review18 from "../../../assets/Review/review (18).svg"
+import Review19 from "../../../assets/Review/review (19).svg"
 
 export function Review() {
   return (
@@ -32,52 +31,148 @@ export function Review() {
           <div className="col">
             <div className="row logos">
               <div className="logos-slide">
-                {/* First set of images */}
-                {[...Array(10)].map((_, i) => (
-                  <Image 
-                    key={`first-${i}`}
-                    src={`../../../assets/Review/review (${i + 1}).svg`}
-                    alt={`Review ${i + 1}`}
+                <Image 
+                    src={Review1}
+                    alt={`Review`}
                     width={100}  // Add appropriate width
                     height={40}  // Add appropriate height
                     className="mx-2"
-                  />
-                ))}
-                {/* Duplicate for seamless animation */}
-                {[...Array(10)].map((_, i) => (
-                  <Image 
-                    key={`dup-first-${i}`}
-                    src={`../../../assets/Review/review (${i + 1}).svg`.trim()}
-                    alt={`Review ${i + 1}`}
-                    width={100}
-                    height={40}
+                />
+                <Image 
+                    src={Review2}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
                     className="mx-2"
-                  />
-                ))}
+                />
+                <Image 
+                    src={Review3}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                <Image 
+                    src={Review4}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                <Image 
+                    src={Review5}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                <Image 
+                    src={Review6}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                 <Image 
+                    src={Review7}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                 <Image 
+                    src={Review8}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                 <Image 
+                    src={Review9}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                 <Image 
+                    src={Review10}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
               </div>
               <div className="logos-slide">
-                {/* Second set of images */}
-                {[...Array(9)].map((_, i) => (
-                  <Image 
-                    key={`second-${i}`}
-                    src={`../../../assets/Review/review (${i + 1}).svg`.trim()}
-                    alt={`Review ${i + 11}`}
-                    width={100}
-                    height={40}
+                <Image 
+                    src={Review10}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
                     className="mx-2"
-                  />
-                ))}
-                {/* Duplicate for seamless animation */}
-                {[...Array(9)].map((_, i) => (
-                  <Image 
-                    key={`dup-second-${i}`}
-                    src={`/assets/Review/review (${i + 11}).svg`.trim()}
-                    alt={`Review ${i + 11}`}
-                    width={100}
-                    height={40}
+                />
+                <Image 
+                    src={Review11}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
                     className="mx-2"
-                  />
-                ))}
+                />
+                <Image 
+                    src={Review12}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                <Image 
+                    src={Review13}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                <Image 
+                    src={Review14}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                <Image 
+                    src={Review15}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                <Image 
+                    src={Review16}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                <Image 
+                    src={Review17}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                <Image 
+                    src={Review18}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
+                <Image 
+                    src={Review19}
+                    alt={`Review`}
+                    width={100}  // Add appropriate width
+                    height={40}  // Add appropriate height
+                    className="mx-2"
+                />
               </div>
             </div>
             <div className="fades fades-left"></div>
